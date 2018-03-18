da0c9ce1e2fdb82d8c4436eadd676092f6c2686c
Alex Entringer <aentringer@users.noreply.github.com>
2018-03-17 18:17:27 -0700

    Improved Readability of Arguments

--- Apple/iTunes/Uninstall-iTunes.ps1
+++ Apple/iTunes/Uninstall-iTunes.ps1
@@ -5,13 +5,22 @@ Stop-Process -Name iTunesHelper -Force -Confirm:$false
 Stop-Process -Name AppleMobileDeviceService -Force -Confirm:$false
 
 # Uninstall iTunes
-$iTunesArgs = '/X', '{D7D4465C-B3B6-4BC1-B336-2803FB57BFAF}', '/qn', 'REBOOT=ReallySuppress'
+$iTunesArgs = @('/X'
+                '{D7D4465C-B3B6-4BC1-B336-2803FB57BFAF}'
+                '/qn'
+                'REBOOT=ReallySuppress')
 Start-Process -FilePath 'msiexec.exe' -ArgumentList $iTunesArgs -Wait
 
 # Uninstall Apple Mobile Device Support
-$AMDSArgs = '/X', '{BD6778C5-6FA5-492A-ADD6-E706339C2A7B}', '/qn', 'REBOOT=ReallySuppress'
+$AMDSArgs = @('/X'
+              '{BD6778C5-6FA5-492A-ADD6-E706339C2A7B}'
+              '/qn'
+              'REBOOT=ReallySuppress')
 Start-Process -FilePath 'msiexec.exe' -ArgumentList $AMDSArgs -Wait
 
 # Uninstall Apple Application Support
-$ASUWArgs = '/X', '{F0C4B709-8BF4-4A72-B527-12E7BF5482F8}', '/qn', 'REBOOT=ReallySuppress'
+$ASUWArgs = @('/X'
+              '{F0C4B709-8BF4-4A72-B527-12E7BF5482F8}'
+              '/qn'
+              'REBOOT=ReallySuppress')
 Start-Process -FilePath 'msiexec.exe' -ArgumentList $ASUWArgs -Wait
