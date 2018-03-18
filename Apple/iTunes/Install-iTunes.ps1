f4ed1216d3792a57c56007324c625c20ed74cb01
Alex Entringer <aentringer@users.noreply.github.com>
2018-03-17 18:16:04 -0700

    Improved Readability of Arguments

--- Apple/iTunes/Install-iTunes.ps1
+++ Apple/iTunes/Install-iTunes.ps1
@@ -1,11 +1,29 @@
-$AASArgs = '/i', "$PSScriptRoot\AppleApplicationSupport64.msi", '/qn', 'REBOOT=ReallySuppress'
+$AASArgs = @('/i'
+             "$PSScriptRoot\AppleApplicationSupport64.msi"
+             '/qn'
+             'REBOOT=ReallySuppress')
 Start-Process -FilePath "msiexec.exe" -ArgumentList $AASArgs -Wait
 Start-Sleep -Seconds 5
 
-$AMDArgs = '/i', "$PSScriptRoot\AppleMobileDeviceSupport6464.msi", '/qn', 'REBOOT=ReallySuppress'
+$AMDArgs = @('/i'
+             "$PSScriptRoot\AppleMobileDeviceSupport6464.msi"
+             '/qn'
+             'REBOOT=ReallySuppress')
 Start-Process -FilePath "msiexec.exe" -ArgumentList $AMDArgs -Wait
 Start-Sleep -Seconds 5
 
-$iTunesArgs = '/i', "$PSScriptRoot\iTunes64.msi", '/qn', 'REBOOT=ReallySuppress', 'DESKTOP_SHORTCUTS=0', 'DISABLEADVTSHORTCUTS=0', 'SCHEDULE_ASUW=0', 'AMDS_IS_INSTALLED=1', 'BONJOUR_IS_INSTALLED=1', 'IAcceptLicense=Yes', 'NO_ASUW=0', 'NO_BONJOUR=0', 'REGSRCH_DESKTOP_SHORTCUTS=0'
+$iTunesArgs = @('/i'
+                "$PSScriptRoot\iTunes64.msi"
+                '/qn'
+                'REBOOT=ReallySuppress'
+                'DESKTOP_SHORTCUTS=0'
+                'DISABLEADVTSHORTCUTS=0'
+                'SCHEDULE_ASUW=0'
+                'AMDS_IS_INSTALLED=1'
+                'BONJOUR_IS_INSTALLED=1'
+                'IAcceptLicense=Yes'
+                'NO_ASUW=0'
+                'NO_BONJOUR=0'
+                'REGSRCH_DESKTOP_SHORTCUTS=0')
 Start-Process -FilePath "msiexec.exe" -ArgumentList $iTunesArgs -Wait
 Start-Sleep -Seconds 5
